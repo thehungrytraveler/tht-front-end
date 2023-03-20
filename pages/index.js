576655b689b100aede3168ebd4716cae49166595
nuwanrg <nuwan_rajitha@outlook.com>
2023-03-20 11:36:53 +0800

    integrated with backend

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { useRef, useState } from 'react';
+import axios from "axios"
 
 export default function Home() {
+  const handleSubmit = e => {
+    // Prevent the default submit and page reload
+    e.preventDefault()
+    console.log('prompt ', prompt);
+    // Handle validations
+    axios
+      .get("https://tht-service.onrender.com/web/processText", { params: {text:prompt })
+      //.get("http://localhost:3000/web/processText", { params: {text:prompt } })
+      .then((response) => {
+        console.log("return came")
+        console.log(response.data)
+        setAnswer(response.data);
+        // Handle response
+      })
+  }
+
+  const [prompt, setPrompt] = useState()
+  const [answer, setAnswer] = useState()
+
   const inputRef = useRef(null);
 
   const [updated, setUpdated] = useState('');
@@ -26,19 +46,19 @@ export default function Home() {
         </h1>
         <div>
         <label>
-      Write your prompt:
+      Write your prompts:
       <br/>
-      <textarea name="postContent" rows={4} cols={100} />
+      <textarea id="prompt" name="prompt" value={prompt} rows={4} cols={100} onChange={e => setPrompt(e.target.value)}/>
     </label>
 
     <br/>
 
-      <button onClick={handleClick}>Submit</button>
+      <button onClick={handleSubmit}>Submit</button>
       <br/><br/>
       <label>
       Answer:
       <br/><br/>
-      <textarea name="postContent" rows={20} cols={100} />
+      <textarea id="answer" name="answer" value={answer}  rows={20} cols={100} />
     </label>
     </div>
       </main>
